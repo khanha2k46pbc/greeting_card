15eb2adf41f9a42f7e9ab2b497584a2dd7fc2b20
Khanh <khanha2k46pbc@gmail.com>
2023-06-05 20:57:32 +0700

    hoàn thành chức năng đăng ký tài khoản

--- accounts/views.py
+++ accounts/views.py
@@ -3,28 +3,76 @@ from django.http import HttpResponse
 from . import models
 from django.template import loader
 from django.http import Http404
+
+
 # Create your views here.
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         # Authenticate user
         try:
             user = models.User.objects.get(username=username, password=password)
-            
-            template = loader.get_template('accounts/go_to_home.html')
+
+            template = loader.get_template("accounts/go_to_home.html")
             context = {}
             return HttpResponse(template.render(context, request))
         except models.User.DoesNotExist:
             # Incorrect username or password
-            error_message = 'Username or password is incorrect.'
-            return render(request, 'accounts/login.html', {'error_message': error_message})
-            
-    return render(request, 'accounts/login.html')
+            error_message = "Username or password is incorrect."
+            return render(
+                request, "accounts/login.html", {"error_message": error_message}
+            )
+
+    return render(request, "accounts/login.html")
+
 
 def register(request):
-    return HttpResponse("Đăng ký, chưa hoàn thiện")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+        address = request.POST["address"]
+
+        username_flag = False
+        email_flag = False
+        context = {}
+        # Check if username is already taken
+        try:
+            user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            username_flag = True
+        # Check if email is already taken
+        try:
+            user = models.User.objects.get(email=email)
+        except models.User.DoesNotExist:
+            email_flag = True
+        if username_flag == False:
+            context["error_username"] = "Username is already taken."
+        if email_flag == False:
+            context["error_email"] = "Email is already taken."
+
+        if username_flag == False or email_flag == False:
+            return render(request, "accounts/register.html", context)
+
+        # Create new user
+        user = models.User(
+            username=username,
+            password=password,
+            email=email,
+            phone=phone,
+            address=address,
+        )
+        user.save()
+        return render(
+            request,
+            "accounts/login.html",
+            {"success_message": "Register successfully."},
+        )
+    return render(request, "accounts/register.html")
+
 
 def setting(request, user_id):
     try:
@@ -33,14 +81,17 @@ def setting(request, user_id):
         raise Http404("Tài khoản không tồn tại")
     return HttpResponse("setting tài khoản %s, chưa hoàn thiện" % user.username)
 
+
 def uploaded_images(request, user_id):
     try:
         user = models.User.objects.get(id=user_id)
     except models.User.DoesNotExist:
         raise Http404("Tài khoản không tồn tại")
-    
+
     httpResponse = HttpResponse()
-    httpResponse.writelines("Xem ảnh đã được upload, tài khoản %s, chưa hoàn thiện <br/>" % user.username)
+    httpResponse.writelines(
+        "Xem ảnh đã được upload, tài khoản %s, chưa hoàn thiện <br/>" % user.username
+    )
     upload_images = models.Upload_image.objects.filter(user=user)
     for upload_image in upload_images:
         # Access the Upload_image attributes
@@ -55,9 +106,11 @@ def stored_images(request, user_id):
         user = models.User.objects.get(id=user_id)
     except models.User.DoesNotExist:
         raise Http404("Tài khoản không tồn tại")
-    
+
     httpResponse = HttpResponse()
-    httpResponse.writelines("Xem ảnh đã được lưu, tài khoản %s, chưa hoàn thiện <br/>" % user.username)
+    httpResponse.writelines(
+        "Xem ảnh đã được lưu, tài khoản %s, chưa hoàn thiện <br/>" % user.username
+    )
     stored_images = models.Stored_image.objects.filter(user=user)
     for stored_image in stored_images:
         # Access the Upload_image attributes
@@ -66,15 +119,15 @@ def stored_images(request, user_id):
 
     return httpResponse
 
+
 def upload(request, user_id):
     try:
         user = models.User.objects.get(id=user_id)
     except:
         return HttpResponse("Tài khoản không tồn tại")
-    
-    template = loader.get_template('accounts/upload.html')
-    context = {
-        'state': 'chưa hoàn thiện',
-    }
-    return HttpResponse(template.render(context, request))  
 
+    template = loader.get_template("accounts/upload.html")
+    context = {
+        "state": "chưa hoàn thiện",
+    }
+    return HttpResponse(template.render(context, request))
