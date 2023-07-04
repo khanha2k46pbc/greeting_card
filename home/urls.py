e6942624f2e177c848fdb961974d4e15c9092b18
Khanh <khanha2k46pbc@gmail.com>
2023-07-04 22:25:13 +0700

    .

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("editor/", views.editor, name="editor"),
+    path('', views.home, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,6 +4,11 @@ from . import models
 from django.template import loader
 
 # Create your views here.
+def editor(request):
+    template = loader.get_template('home/editor.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
 def home(request):
     template = loader.get_template('home/home.html')
     context = {}
